1163ad8377b77878064ea053ca780b37850cdc95
Blizonaire <Necesku@gmail.com>
2023-07-16 11:37:23 +0200

    Just moving auth things to another file

---
+++ backend/auth/auth.go
@@ -0,0 +1,47 @@
+package auth
+
+import "github.com/joho/godotenv";
+import "github.com/golang-jwt/jwt";
+import "golang.org/x/crypto/bcrypt";
+import "fmt";
+import "os";
+
+func GetFromEnv(key string) string {
+	err := godotenv.Load("../config.env");
+	if err != nil {
+		fmt.Println("An error happened while loading env file!");
+	}
+	return os.Getenv(key);
+}
+
+func CreateToken(user string, password string) (string, error) {
+	jwt_secret := GetFromEnv("ENV_SECRET");
+	token_raw := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user": user,
+		"authorized": true,
+	});
+	token, err := token_raw.SignedString([]byte(jwt_secret));
+	if err != nil {
+		return "", err;
+	}
+	return token, nil;
+}
+
+func Hash(pass string) string {
+	hashed, _ := bcrypt.GenerateFromPassword([]byte(pass), 8); return string(hashed);
+}
+
+func GetUserFromToken(tokenString string) (jwt.MapClaims, error) {
+	jwt_secret := GetFromEnv("ENV_SECRET");
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Invalid signing method!");
+		}
+		return []byte(jwt_secret), nil;
+	});
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil;
+	} else {
+		return nil, err;
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -2,65 +2,18 @@ package main
 
 import "fmt";
 import "github.com/gofiber/fiber/v2";
-import "github.com/joho/godotenv";
-import "github.com/golang-jwt/jwt"
-import "os";
-import "golang.org/x/crypto/bcrypt";
-
-func getFromEnv(key string) string {
-	err := godotenv.Load("config.env");
-	if err != nil {
-		fmt.Println("An error happened while loading env file!");
-	}
-	return os.Getenv(key);
-}
-
-func createToken(user string, password string) (string, error) {
-	jwt_secret := getFromEnv("ENV_SECRET");
-	token_raw := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"user": user,
-		"authorized": true,
-	});
-	claims := token_raw.Claims.(jwt.MapClaims);
-	claims["user"] = user;
-	claims["password"] = password;
-	claims["authorized"] = true;
-	token, err := token_raw.SignedString([]byte(jwt_secret));
-	if err != nil {
-		return "", err;
-	}
-	return token, nil;
-}
-
-func hash(pass string) string {
-	hashed, _ := bcrypt.GenerateFromPassword([]byte(pass), 8); return string(hashed);
-}
-
-func getUserFromToken(tokenString string) (jwt.MapClaims, error) {
-	jwt_secret := getFromEnv("ENV_SECRET");
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Invalid signing method!");
-		}
-		return []byte(jwt_secret), nil;
-	});
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
-		return claims, nil;
-	} else {
-		return nil, err;
-	}
-}
+import "teletvbis/panelactyl/backend/auth";
 
 func main() {
 
 	// JWT (createToken, getUserFromToken), HASHING (hash) TEST
 
-	token, err := createToken("test", hash("hello!"));
+	token, err := auth.CreateToken("test", auth.Hash("hello!"));
 	if err != nil {
 		panic(err);
 	}
 
-	claims, err := getUserFromToken(token);
+	claims, err := auth.GetUserFromToken(token);
 	if err != nil {
 		panic(err);
 	}
